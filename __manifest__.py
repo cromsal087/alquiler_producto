0026f4e4b1cf4c30445ac0fe0de0f243f2b10547
cloe <cromsal087@g.educaand.es>
2025-02-11 10:59:44 +0100

    dependencias del manifest corregidas

--- __manifest__.py
+++ __manifest__.py
@@ -5,11 +5,10 @@
  'author': 'Cloe Romero',
  'category': 'Custom',
  'summary': 'Gestión de alquileres de producto',
- 'depends': ['base', 'customer','product'],
+ 'depends': ['base', 'sale_management'],
  'data': [
  'security/ir.model.access.csv',
  'views/alquiler_producto_views.xml',
-   'data/cron.xml',
  ],
  'icon': '/alquiler_producto/static/description/rent.png',
  'installable': True,
